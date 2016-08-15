0bc63cad23c92ffd1930d8d444f397f16e3c1663
Enzo Ferey <shoon96@gmail.com>
2016-08-15 22:20:44 +0200

    Search input working forth and back

--- index.js
+++ index.js
@@ -9,16 +9,40 @@ var main = function() {
 	});
 
 	$(document).mouseup(function(e) {
-    var container = $('#side-menu-collapsable');
-    if(container.is(':visible') && !container.is(e.target) && container.has(e.target).length === 0){
-      container.animate({
-        left: "-200px"
-      },
-      100, function(){
-        container.hide();
-      });
-    }
-  });
+	    var container = $('#side-menu-collapsable');
+	    if(container.is(':visible') && !container.is(e.target) && container.has(e.target).length === 0){
+		    container.animate({
+		    	left: "-200px"
+		    },
+		    100, function(){
+		    	container.hide();
+		    });
+		}
+		else {
+			var input = $('#search-input-input');
+			if(input.is(':visible') && !container.is(e.target) && container.has(e.target).length === 0) {
+				input.attr('placeholder', '');
+      			input.val('');
+				$('#search-input').animate({
+			        width: "50px"
+			    }, 300, function (){
+			    	input.hide();
+			    });
+			}
+		}
+	});
+
+	$('#search-icon').click(function() {
+		var input = $('#search-input-input');
+		input.show();
+		input.attr('placeholder', 'Enter your search term...');
+		$('#search-input').animate({
+	        width: "240px"
+	    },
+	      300, function(){ // On finish
+	          input.focus();
+	    });
+	});
 }
 
 $(document).ready(main);
