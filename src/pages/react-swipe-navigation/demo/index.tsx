a4e1389ddcba9f2df33983e1ad2ec25a9287c280
Enzo Ferey <hello@enzoferey.com>
2023-03-12 11:07:08 +0100

    Fix package build issue.

--- src/pages/react-swipe-navigation/demo/index.tsx
+++ src/pages/react-swipe-navigation/demo/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 
 const DemoPage: NextPage = (props) => {
   React.useEffect(() => {
-    import("./package.js");
+    import("@/react-swipe-navigation/package.js");
   }, []);
 
   return <div id="app" style={{ height: "100vh" }}></div>;
